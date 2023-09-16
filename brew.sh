67f84c2b2965d1f4a44d8a98d3b0468ff546f003
Kiki Dawson <kikiadawson@gmail.com>
2023-09-16 18:57:44 +0100

    add more packages

--- brew.sh
+++ brew.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-echo "installing aws-cli..."
-brew install awscli
+echo "install brew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo "installing terraform..."
+echo "installing vscode..."
+brew install --cask visual-studio-code
+brew install --cask google-chrome
+
+echo "installing platform tools..."
+brew install awscli
 brew install terraform
+brew install terragrunt
+brew install warrensbox/tap/tfswitch
+brew install warrensbox/tap/tgswitch
+
+echo "installing container tools..."
+brew install docker
+brew install kubectl
+brew install kubectx
+
+echo "installing communication tools..."
+brew install --cask slack
+brew install --cask discord
+brew install --cask zoom
